ebba61a53511cdd77f7a70e9b814b3e018d8196d
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2013-11-09 02:34:31 -0200

    Incluido novos scripts

--- install.sublime.text2.sh
+++ install.sublime.text2.sh
@@ -5,8 +5,8 @@
 # 11/2013
 
 APLICACAO="Sublime Text 2.0.2"
-PATH_INSTALACAO=
-
+PATH_INSTALACAO=/usr/lib
+DIR_INSTALACAO=SublimeText2
 
 # verifica se o usuario tem permissoes root
 if  [ "$(id -u)" != "0" ];  then
@@ -15,7 +15,7 @@ if  [ "$(id -u)" != "0" ];  then
 fi
 
 # solicita confirmacao da instalacao
-read -p "   Deseja realmente instalar o ${APLICACAO}  [s/N] " resp
+read -p "Deseja realmente instalar o ${APLICACAO}  [s/N] " resp
 resp=$(echo "$resp" | tr [[:upper:]] [[:lower:]] | cut -c1)
 
 if [ "$resp" != "s" ]; then
@@ -25,31 +25,70 @@ fi
 echo "Iniciando a instalacao do ${APLICACAO}"
 
 # verifica a plataforma para fazer o download
+
 if [ "$(uname -m)" == "x86_64" ]; then
-  # 64 bit
+  # 64 bit	
   URL=http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+  FILE_TMP=Sublime\ Text\ 2.0.2x64.tar.bz2	
 else
   # 32 bit
-  URL=http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.tar.bz2
+  URL="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.tar.bz2" 
+  FILE_TMP=Sublime\ Text\ 2.0.2.tar.bz2	
 fi
 
-# download e descompactacao para o diretorio de instalacao
-file=$(basename $URL)
-wget ${URL} -P /tmp
+FILE=SublimeText2.tar.bz2
 
-[ -d "${PATH_INSTALACAO}" ] && rm -rf ${PATH_INSTALACAO}
+if [[ ! -f /tmp/$FILE_TMP ]]; then
+  wget ${URL} -P /tmp  
+fi
+	
 
-mkdir -p  ${PATH_INSTALACAO}
-chown -R root:root ${PATH_INSTALACAO}
-chmod -R +r ${PATH_INSTALACAO}
+if [ "$(uname -m)" == "x86_64" ]; then
+  # 64 bit	
+  mv /tmp/Sublime\ Text\ 2.0.2x64.tar.bz2 /tmp/$FILE	
+else
+  # 32 bit
+  mv /tmp/Sublime\ Text\ 2.0.2.tar.bz2 /tmp/$FILE
+fi
 
-tar jxvf /tmp/$file ${PATH_INSTALACAO}
+tar jxvf /tmp/$FILE -C /tmp
+mv /tmp/Sublime\ Text\ 2 /tmp/${DIR_INSTALACAO}
 
-# criar o startes
+[ -d "${PATH_INSTALACAO}/${DIR_INSTALACAO}" ] && rm -rf ${PATH_INSTALACAO}/${DIR_INSTALACAO}
 
-# limpar os diretorios
+mkdir -p  ${PATH_INSTALACAO}/${DIR_INSTALACAO}
+chown -R root:root ${PATH_INSTALACAO}/${DIR_INSTALACAO}
+chmod -R +r ${PATH_INSTALACAO}/${DIR_INSTALACAO}
 
-[ -f /tmp/$file ] && rm /tmp/$file
+mv /tmp/${DIR_INSTALACAO} $PATH_INSTALACAO
+
+[[ ! -h /usr/bin/sublime ]] && ln -s /${PATH_INSTALACAO}/${DIR_INSTALACAO}/sublime_text /usr/bin/sublime
+
+Starter=/usr/share/applications/sublime.desktop
+
+cat <<EOF>> ${Starter}
+[Desktop Entry]
+Version=1.0
+Name=Sublime Text 2
+GenericName=Text Editor
+ 
+Exec=sublime
+Terminal=false
+Icon=${PATH_INSTALACAO}/${DIR_INSTALACAO}/Icon/48x48/sublime_text.png
+Type=Application
+Categories=TextEditor;IDE;Development
+X-Ayatana-Desktop-Shortcuts=NewWindow
+ 
+[NewWindow Shortcut Group]
+Name=New Window
+Exec=sublime -n
+TargetEnvironment=Unity
+EOF
+ 
+chmod +r ${Starter}
+
+
+[[ -f /tmp/$FILE ]] && rm /tmp/$FILE
 
 
 
